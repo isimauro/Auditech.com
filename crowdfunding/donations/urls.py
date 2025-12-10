8bd2170a2677372157c7385087740dc92914337f
ykl08 <yakelavicente@gmail.com>
2025-12-10 20:22:48 +0100

    Delete crowdfunding directory

--- crowdfunding/donations/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('create/<slug:slug>/', views.create_donation, name='donation-create'),
-    path('my-donations/', views.user_donations, name='user-donations'),
-    path('success/<int:donation_id>/', views.donation_success, name='donation-success'),
-  #  path('webhook/stripe/', views.stripe_webhook, name='stripe-webhook'),
-]
